b66b20a068f3b95b0640426bd7561dafceeebaac
Smith Ringtho <sringtho@gmail.com>
2024-05-08 20:40:46 +0300

    Refactor: Updated the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Footer.scss'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
 
 const Footer = () => {
   return (
-    <footer className='footer_container'>Footer</footer>
+    <footer className="footer_container">
+      <small>Designed by Smith Ringtho</small>
+      <div className='footer_logos'>
+        <a href="https://github.com/ringtho">
+          <GitHubIcon className='logo' />
+        </a>
+        <a href="https://www.linkedin.com/in/sringtho/">
+          <LinkedInIcon className='logo' />
+        </a>
+      </div>
+    </footer>
   )
 }
 
